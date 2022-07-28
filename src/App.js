e1159406a6f89342cbbc292a0726cbe51666dda2
mat-villalba <matiasleonardo.villalba@nttdata.com>
2022-07-27 21:11:16 -0300

    calendario funcionando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import 'date-fns'
+import Grid from '@material-ui/core/Grid'
+import DateFnsUtils from '@date-io/date-fns'
+import{
+  MuiPickersUtilsProvider,
+/*   KeyboardTimePicker,
+  KeyboardDatePicker */
+  DatePicker
+} from '@material-ui/pickers'
+import { useState } from 'react';
+import esLocale from 'date-fns/locale/es';
+
 
 function App() {
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
+      <div style={{ margin:"5%"}}>
+      <DatePicker
+        disableFuture
+        openTo='date'
+        format='dd/MM/yyyy'
+        label='Seleccione la fecha'
+        views={['year', 'month', 'date']}
+        value={selectedDate}
+        onChange={handleDateChange}
+        variant='dialog'
+        cancelLabel='Cancelar'
+        allowKeyboardControl={true}
+      />
+      </div>
+    </MuiPickersUtilsProvider>
   );
 }
 
 export default App;
+
+/* function App() {
+
+const [selectedDate, setSelectedDate] = useState(
+  new Date("2020-09-11T12:00:00")
+)
+
+const handleDateChange = (date) => {
+  setSelectedDate(date)
+}
+
+  return (
+    <div className="App">
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify='space-around'>
+          <KeyboardDatePicker 
+          disableToolbar
+          variant='dialog'
+          format='MM/dd/yyy'
+          margin='normal'
+          id='date-picker'
+          label='Date Picker'
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date'
+          }}
+          />
+
+          <KeyboardTimePicker
+            margin='normal'
+            id='time-picker'
+            label="Time Picker"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date'
+            }}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
+    </div>
+  );
+}
+
+export default App;*/
